7aa79240097b6f655edb7d90815bb7a23f662d32
Holiver98 <68225517+Holiver98@users.noreply.github.com>
2020-07-17 14:11:34 +0200

    Set helyett List az adatbázisban

--- PizzaApp/src/main/java/database/InMemoryDatabase.java
+++ PizzaApp/src/main/java/database/InMemoryDatabase.java
@@ -2,22 +2,23 @@ package database;
 
 import model.*;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class InMemoryDatabase {
-    public Set<User> users;
-    public Set<Rating> ratings;
-    public Set<Pizza> pizzas;
-    public Set<Order> orders;
-    public Set<Ingredient> ingredients;
+    public List<User> users;
+    public List<Rating> ratings;
+    public List<Pizza> pizzas;
+    public List<Order> orders;
+    public List<Ingredient> ingredients;
 
     public InMemoryDatabase(){
-        users = new HashSet<User>();
-        ratings = new HashSet<Rating>();
-        pizzas = new HashSet<Pizza>();
-        orders = new HashSet<Order>();
-        ingredients = new HashSet<Ingredient>();
+        users = new ArrayList<User>();
+        ratings = new ArrayList<Rating>();
+        pizzas = new ArrayList<Pizza>();
+        orders = new ArrayList<Order>();
+        ingredients = new ArrayList<Ingredient>();
     }
 }
